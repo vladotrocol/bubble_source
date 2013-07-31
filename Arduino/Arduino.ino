ce5ce33f5f23951a5c5e577abce9d3b2afa25410
Vlad Otrocol <vladotrocol@yahoo.com>
2013-07-31 15:51:10 +0100

    arduino working somehow

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,19 +1,62 @@
+#include <Servo.h> 
+
+Servo servoPump;
+Servo servoSwing;
 int incomingByte = 0;
 int led = 13;
 void setup() {                
   Serial.begin(9600);
-  pinMode(led, OUTPUT); 
+  servoPump.attach(4);
+  servoSwing.attach(5);
+  pinMode(led, OUTPUT);
+  servoSwing.write(90);
+  servoPump.write(145);
 }
 
+
+int pos = 0;
+
 void loop() {
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-    delay(1000);
-  if (Serial.available() > 0) {
-    incomingByte = Serial.read();
-    Serial.print("I received: ");
-    Serial.println(incomingByte, DEC);
-      digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000); 
+  digitalWrite(led, LOW);
+  delay(1000);
   
+  if (Serial.available() >0) {
+    incomingByte = Serial.read();
+    digitalWrite(led, HIGH);
+    delay(1000);   
+    if(incomingByte == 'g'){ //'g'
+      
+      //move in position for smoke
+      servoSwing.write(40);
+      delay(1400);
+      
+      //suck smoke
+      servoPump.write(20);
+      delay(600);
+      
+      //dip into solution
+      servoSwing.write(90);
+      delay(1400);
+      
+      //swing into bubble making position
+      servoSwing.write(140);
+      delay(140);
+      
+      //slow blow
+      for(pos = 21; pos<=180; pos+=1){ 
+        servoPump.write(pos);
+        delay(30);
+      }
+      
+      //fast blow
+      servoPump.write(145);
+      delay(600);
+      
+      //return to initial stage
+      servoSwing.write(90);
+      delay(600);
+      servoPump.write(145);
+      delay(600);
+    }
   }
 }
